dd98833c1818fb31d43730efd90e9e6782c0e2e0
dkzerfu <dkzerfu@gmail.com>
2021-01-29 12:03:04 -0500

    homework

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log('Hello, front end');
+
+const btn = document.querySelector('#btn');
+// click button
+btn.onclick = function () {
+    const selector = document.querySelectorAll('input[name="temp"]');
+    let selectedValue;
+    for (const a of selector) {
+        if (a.checked) {
+            selectedValue = a.value;
+            break;
+        }
+    }
+    alert(selectedValue);
+
+function cToF() {
+    var c = document.getElementById("tempinput").value;
+  return (c * 9 / 5) + 32;
+}
+
+function fToC() {
+    var f = document.getElementById("tempinput").value;
+  return (f - 32) * 5 / 9;
+}
