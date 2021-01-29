f58fce69fe8ceb5ada324a6c163eb29ec492d859
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-01-28 20:33:22 -0800

    Updates JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 console.log('Hello, front end');
+
+function mySubmit() {
+    document.getElementById("myForm").submit()
+  
+}
+
+const subButton = document.querySelector(".submitButton");
+subButton.addEventListener("click", function(e) {
+    e.preventDefault()
+    let input = document.querySelector(".directInput").value;
+    let far = document.querySelector("#fahrenheit")
+    let cel = document.querySelector("#celcius")
+    let result = ''
+        if (far.checked === true && cel.checked === false) {
+            result = (input - 30) / 2 
+        }
+        if (cel.checked === true && far.checked === false){
+            result  = (input + 30) * 2
+        } console.log(result)
+})
