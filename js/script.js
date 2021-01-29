f44caf979ac97740ba08bf2045ab453937a7b884
semuel1 <semuelst1@gmail.com>
2021-01-29 09:54:00 -0800

    updates

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log('Hello, front end');
+document.querySelector('#submit').addEventListener('click', function () {
+  const inputTemp =  document.querySelector('input').value
+  
+
+  const outputTemp = (inputTemp - 32) / 1.8
+
+  document.querySelector('.display').innerText = outputTemp
+
+})
