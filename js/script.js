0aed0e366f2d2aee59673d1772533b1745757086
gndm <demoura.gabe@gmail.com>
2021-01-28 23:53:25 -0500

    Made severa l changes to index.html - stylesheet - js file ... functional app

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log('Hello, front end');
+const button = document.getElementById("submit");
+console.log(button);
+
+button.addEventListener("click", function () {
+    const userInput = document.getElementById("input").value;
+    let conversion = (userInput - 32) * (5/9);
+    let finalConv = Math.round(conversion);
+    document.getElementById("output").innerText = finalConv;
+    console.log(conversion);
+  });
+
+  const button2 = document.getElementById("clear");
+
+  function clearInput (){
+    const userInput = document.getElementById("input").value = '';
+    document.getElementById("output").innerText = '';
+  }
+
+  button2.addEventListener("click", clearInput);
+
+
+
+
