6faf2d1c257736eb4b728f2cbb341d6530551845
darrengp <ceo.darrenperry@gmail.com>
2021-01-29 06:37:57 -0800

    Updates to degrees symbol

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ fButton.addEventListener("click", function (a) {
   let b = a * 1.8 + 32;
   b = Math.round(b);
   // console.log(b);
-  document.querySelector(".convert").innerText = `${b}*F`;
+  document.querySelector(".convert").innerText = `${b}°F`;
 });
 /////////////////////////////////////////
 cButton.addEventListener("click", function (a) {
@@ -36,14 +36,14 @@ cButton.addEventListener("click", function (a) {
 
   let b = (a - 32) * 0.5556;
   b = Math.round(b);
-  document.querySelector(".convert").innerText = `${b}*C`;
+  document.querySelector(".convert").innerText = `${b}°C`;
 });
 
 /////////////Clear Button
 const clearB = document.getElementById("clearButton");
 
 clearB.addEventListener("click", function () {
-  alert("WHYWHYWHY");
+  alert("Wait, What, Why?");
   document.querySelector(".convert").innerText = " ";
   document.querySelector("#textArea").value = " ";
 });
