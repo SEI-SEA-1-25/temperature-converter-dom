d32d3c9d88b1d29e2b14c12b972de326c2b7399a
ChrisB007 <chrisbitoy@gmail.com>
2021-01-29 11:00:22 -0500

    temparature converter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('Hello, front end');
+// collecting input from frontend "temp.html"
+
+let finput = document.querySelector("#yourtemp").value;
+// let ftemp = finput.value;
+
+// converting form input to celcius
+let cel = function(ftemp){
+    let celcius = Math.floor((ftemp - 32) * .5556);
+    return celcius;
+}
+
+//Displaying on the frontend "temp.html"
+function celDisplay(){
+    document.querySelector("#celcius").innerHTML = `Temperature in Celcius is: ${finput}`;
+}
+
+// console.log(cel(50)) -- works!!
+
+
