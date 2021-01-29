540e722be3d364f67f80aeff63895841e0b4ad46
bcheney21 <brinn_cheney@icloud.comgit config --global user.email brinn_cheney@icloud.com>
2021-01-29 09:38:52 -0800

    html and css changes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 console.log('Hello, front end');
-document.querySelector('#submit-degree').addEventListener('click', ()=>{
+document.querySelector('#submit-degree').addEventListener('click', ()) =>{ 
     const newInput = document.querySelector('#degree-input').value
     document.querySelector('#degree-input').value = ''
     const display = document.createElement('div')
     document.querySelector('.input-container').append(display)
     display.classList.add('degree')
     display.innerText = newInput
-    // document.getElementById(".twood").innerText = newTwood;
-    // const display = document.querySelector('.twood').value = newTwood
-  })
+  }
  
   
