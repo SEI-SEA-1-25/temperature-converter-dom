224892775acd7c69de454e87b85029b07b3b5dab
bcheney21 <brinn_cheney@icloud.comgit config --global user.email brinn_cheney@icloud.com>
2021-01-29 09:11:27 -0800

    progress so far

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log('Hello, front end');
+document.querySelector('#submit-degree').addEventListener('click', ()=>{
+    const newInput = document.querySelector('#degree-input').value
+    document.querySelector('#degree-input').value = ''
+    const display = document.createElement('div')
+    document.querySelector('.input-container').append(display)
+    display.classList.add('degree')
+    display.innerText = newInput
+    // document.getElementById(".twood").innerText = newTwood;
+    // const display = document.querySelector('.twood').value = newTwood
+  })
+ 
+  
