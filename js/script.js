ff16788c2751f1ebc5275f885df28037c0e21a0e
NBryant83 <nakisha.bryant@gmail.com>
2021-01-29 11:22:12 -0500

    first commit of temp homework

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
-console.log('Hello, front end');
+// console.log('Hello, front end');
+//Fahrenheit to Celsius
+function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.querySelector('#outputCelsius').innerText = (valNum - 32) / 1.8;
+}
+//Fahrenheit to Kelvin
+function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.querySelector('#Kelvin').innerText = ((valNum - 32) / 1.8) + 273.15;
+}
+
+//Celsius to Fahrenheit
+function temperatureConverter2(valNum) {
+    valNum = parseFloat(valNum);
+    document.querySelector('#outputFahrenheit').innerText = (valNum * 1.8) + 32;
+}
+
+//Celsius to Kelvin
+function temperatureConverter2(valNum) {
+    valNum = parseFloat(valNum);
+    document.querySelector('#KelvinC').innerText = (valNum + 273.15);
+}
+//Kelvin to Celsius
+function temperatureConverter3(valNum) {
+    valNum = parseFloat(valNum);
+    document.querySelector('#outputCelsiusK').innerText = (valNum - 273.15);
+}
+
+//Kelvin to Fahrenheit
+function temperatureConverter3(valNum) {
+    valNum = parseFloat(valNum);
+    document.querySelector('#outputFahrenheitK').innerText = ((valNum - 273.15) * 1.8) + 32;
+}
+
+
+document.querySelectorAll('button')
